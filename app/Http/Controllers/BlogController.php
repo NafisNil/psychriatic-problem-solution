b5585daf3b6031d314323be405b362e6bbe33add
nafisnil <nafis.zaman1.an@gmail.com>
2023-09-11 18:04:18 +0600

    firstcommmit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Image;
+use App\Http\Requests\BlogRequest;
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $blog = Blog::orderBy('id', 'desc')->get();
+        $blogCount = Blog::count();
+         return view('backend.blog.index',['blog'=>$blog,'blogCount'=> $blogCount,]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('backend.blog.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BlogRequest $request)
+    {
+        //
+        $blog = Blog::create($request->all());
+       
+        if ($request->hasFile('blog')) {
+            $this->_uploadImage($request, $blog);
+        }
+        return redirect()->route('blog.index')->with('success','Data inserted successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Blog $blog)
+    {
+        //
+        return view('backend.blog.edit',[
+            'edit' => $blog
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BlogRequest $request, Blog $blog)
+    {
+        //
+        $blog->update($request->all());
+       
+        if ($request->hasFile('logo')) {
+            @unlink('storage/'.$blog->logo);
+            $this->_uploadImage($request, $blog);
+        }
+        return redirect()->route('blog.index')->with('success','Data inserted successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+        if(!empty($blog->logo));
+        @unlink('storage/'.$blog->logo);
+       
+        $blog->delete();
+        return redirect()->route('blog.index')->with('status','Data deleted successfully!');
+    }
+
+
+    private function _uploadImage($request, $about)
+    {
+        # code...
+        if( $request->hasFile('logo') ) {
+            $image = $request->file('logo');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(260, 310)->save('storage/' . $filename);
+            $about->logo = $filename;
+            $about->save();
+        }
+       
+    }
+}
